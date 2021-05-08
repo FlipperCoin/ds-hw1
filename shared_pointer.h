3eef710f37ac5db05adf5111b4c63f903facd4af
Itai Spigelman <itai10020@gmail.com>
2021-05-08 12:59:52 +0300

    shared pointer, vector

---
+++ shared_pointer.h
@@ -0,0 +1,69 @@
+//
+// Created by Itai on 17/01/2021.
+//
+
+#ifndef _SHAREDPOINTER_H
+#define _SHAREDPOINTER_H
+namespace ds {
+    template<typename T>
+    class SharedPointer
+    {
+    private:
+        void dispose() {
+            if ((*ref_count)-- <= 1) {
+                if (pointer != nullptr) {
+                    delete pointer;
+                }
+                delete ref_count;
+            }
+        }
+    public:
+        explicit SharedPointer(T* pointer) : pointer(pointer), ref_count(new int(1)) {};
+
+        SharedPointer() : pointer(nullptr), ref_count(new int(0)) {};
+
+        ~SharedPointer() {
+            dispose();
+        }
+
+        SharedPointer(const SharedPointer& other) {
+            ref_count = other.ref_count;
+            pointer = other.pointer;
+
+            (*ref_count)++;
+        }
+
+        SharedPointer& operator=(const SharedPointer& other) {
+            if (&other == this) {
+                return *this;
+            }
+
+            dispose();
+
+            ref_count = other.ref_count;
+            pointer = other.pointer;
+
+            (*ref_count)++;
+
+            return *this;
+        }
+
+        T& operator*() {
+            return *pointer;
+        }
+        const T& operator*() const{
+            return *pointer;
+        }
+        T* operator->() {
+            return pointer;
+        }
+        const T* operator->() const {
+            return pointer;
+        }
+
+    private:
+        T* pointer;
+        int* ref_count;
+    };
+}
+#endif
