76f1dafef00c079b74ceaae94daffb3fedc6c918
Itai Spigelman <itai10020@gmail.com>
2021-05-21 22:40:40 +0300

    documentation

--- car_dealership_manager.cpp
+++ car_dealership_manager.cpp
@@ -48,7 +48,7 @@ StatusType CarDealershipManager::RemoveCarType(int typeID) {
             return FAILURE;
         }
 
-        // remove type from zero grades tree
+        // remove type from zero grades tree in O(log(n))
         ZeroGradeTypeNode zeroNode(typeID);
         ZeroGrades.remove(zeroNode);
 
@@ -81,6 +81,7 @@ void CarDealershipManager::UpdateGrade(CarNode* carNode, int modelID, int oldGra
     carNode->Models[modelID].Grade = newGrade;
 
     // move from non-zero to zero grade (if new grade is zero, old grade def not zero)
+    // O(log(n) + log(m) + log(M)) = O(log(n) + log(M))
     if (newGrade == 0) {
         Grades.remove({carNode->TypeID, modelID, oldGrade});
         auto zeroGradeTypeNode = ZeroGrades.find(ZeroGradeTypeNode(carNode->TypeID));
@@ -89,6 +90,7 @@ void CarDealershipManager::UpdateGrade(CarNode* carNode, int modelID, int oldGra
     }
 
     // move from zero to non-zero grade
+    // O(log(n) + log(m) + log(M)) = O(log(n) + log(M))
     else if(oldGrade == 0) {
         auto zeroGradeTypeNode = ZeroGrades.find(ZeroGradeTypeNode(carNode->TypeID));
         zeroGradeTypeNode->Value.ModelsTree.remove(modelID);
@@ -96,7 +98,7 @@ void CarDealershipManager::UpdateGrade(CarNode* carNode, int modelID, int oldGra
         return;
     }
 
-    // stay non-zero
+    // stay non-zero, O(log(M))
     Grades.remove({carNode->TypeID, modelID, oldGrade});
     Grades.insert({carNode->TypeID, modelID, newGrade});
 }
@@ -123,14 +125,14 @@ StatusType CarDealershipManager::SellCar(int typeID, int modelID) {
 
         ModelData modelData = carNode->Value.Models[modelID];
 
-        // update sells
+        // update sells, O(log(M))
         Sells.remove({.TypeID=typeID, .ModelID=modelID, .Sells = modelData.Sells++});
         Sells.insert({.TypeID=typeID, .ModelID=modelID, .Sells = modelData.Sells});
 
-        // update sells in model data
+        // update sells in model data, O(1)
         carNode->Value.Models[modelID].Sells = modelData.Sells;
 
-        // update best seller for same type id
+        // update best seller for same type id, O(1)
         auto bestSellingModel = carNode->Value.BestSellingModel;
         auto sellsForBestSelling = carNode->Value.Models[bestSellingModel].Sells;
 
@@ -139,7 +141,7 @@ StatusType CarDealershipManager::SellCar(int typeID, int modelID) {
             carNode->Value.BestSellingModel = modelID;
         }
 
-        // update grade
+        // update grade, O(log(n) + log(M))
         int newGrade = modelData.Grade + 10;
         UpdateGrade(&(carNode->Value), modelID, modelData.Grade, newGrade);
     } catch (std::bad_alloc& e) {
@@ -170,6 +172,7 @@ StatusType CarDealershipManager::MakeComplaint(int typeID, int modelID, int t) {
         }
 
         ModelData modelData = carNode->Value.Models[modelID];
+        // O(log(n) + log(M))
         UpdateGrade(&(carNode->Value), modelID, modelData.Grade, modelData.Grade - int(100/t));
 
     } catch (std::bad_alloc& e) {
@@ -185,6 +188,8 @@ StatusType CarDealershipManager::GetBestSellerModelByType(int typeID, int *model
     }
     try {
         if (typeID == 0){
+            // Get highest seller from all models,
+            // first leaf from Sells tree in O(1)
             if (!Sells.isEmpty()) {
                 *modelID = Sells.getSmallestChild()->Value.ModelID;
                 return SUCCESS;
@@ -192,6 +197,7 @@ StatusType CarDealershipManager::GetBestSellerModelByType(int typeID, int *model
             if (Cars.isEmpty()) {
                 return FAILURE;
             }
+            // There are cars but they are all with grade 0, so model id 0 is def correct
             else {
                 *modelID = 0; // doesn't matter the car type, it'll be model id 0
                 return SUCCESS;
@@ -207,6 +213,7 @@ StatusType CarDealershipManager::GetBestSellerModelByType(int typeID, int *model
             // provided type isn't in the cars tree
             return FAILURE;
         }
+        // After finding car node, get best seller in O(1)
         *modelID = carNode->Value.BestSellingModel;
 
     }catch (std::bad_alloc& e){
@@ -219,14 +226,22 @@ StatusType CarDealershipManager::GetWorstModels(int numOfModels, int *types, int
     if (numOfModels <= 0) {
         return INVALID_INPUT;
     }
+
+    // Start from the first leaf of Grades in O(1)
     TreeNode<GradeNode>* iter = Grades.getSmallestChild().rawPointer();
+    // Start from the first leaf of ZeroGrades in O(1)
     TreeNode<ZeroGradeTypeNode>* zero_iter = ZeroGrades.getSmallestChild().rawPointer();
+
+    // Iterate through all models
     for (int i = 0; i < numOfModels; i++){
+        // Start in negative grades from Grades tree, iterate leaves in O(m)
         if(iter != nullptr && iter->Value.Grade < 0){
             types[i] = iter->Value.TypeID;
             models[i] = iter->Value.ModelID;
             iter = iter->Next;
         }
+        // When no negative grades left go through zero grades,
+        // iterating through ZeroGrades leaves and leaves of the models tree in each node in O(m)
         else if(zero_iter != nullptr){
             // need a loop for every zero grade node that goes through all nodes in tree - zero models
             auto models_iter = zero_iter->Value.ModelsTree.getSmallestChild().rawPointer();
@@ -240,6 +255,8 @@ StatusType CarDealershipManager::GetWorstModels(int numOfModels, int *types, int
             i--;
             zero_iter = zero_iter->Next;
         }
+        // When no zero grades left go through positive grades,
+        // iterate leaves in O(m)
         else if(iter != nullptr){
             types[i] = iter->Value.TypeID;
             models[i] = iter->Value.ModelID;
